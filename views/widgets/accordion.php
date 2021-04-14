cbd02eb0dd25517768b44b2fc739d1f9030f7675
Khizar <khizarhasan927@gmail.com>
2021-04-14 16:17:29 +0530

    testimonials

--- views/widgets/accordion.php
+++ views/widgets/accordion.php
@@ -16,35 +16,38 @@ if(!$bellows) {
 
 ?>
 
-<section class="selleradiseAccordion" x-data="{opened: '0'}">
+<section class="selleradise_Accordion" x-data="{opened: '0'}">
 
   <?php if($settings['section_title']): ?>
-    <div class="sectionTitle">
+    <div class="selleradise_Accordion__title">
       <h2><?php echo $settings['section_title']; ?></h2>
     </div>
   <?php endif; ?>
 
   <?php foreach($bellows as $key => $bellow): ?>
 
-    <div class="bellow" x-bind:class="{'isOpen': opened === '<?php echo esc_attr($key); ?>'}">
+    <div class="selleradise_Accordion__bellow" x-bind:class="{'selleradise_Accordion__bellow--open': opened === '<?php echo esc_attr($key); ?>'}">
       <button 
-        class="trigger"
-        id="selleradiseAccordion--trigger-<?php echo esc_attr($key); ?>"
-        aria-controls="selleradiseAccordion--description-<?php echo esc_attr($key); ?>"
+        id="selleradise_Accordion__trigger-<?php echo esc_attr($key); ?>"
+        aria-controls="selleradise_Accordion__description-<?php echo esc_attr($key); ?>"
         x-on:click="opened !== '<?php echo esc_attr($key); ?>' ? opened = '<?php echo esc_attr($key); ?>' : opened = null;"
         x-bind:aria-expanded="opened === '<?php echo esc_attr($key); ?>' ? true : false">
-        <h3 class="title">
+
+        <span>
+          <?php echo selleradise_widgets_svg('material/chevron-down'); ?>
+        </span>
+
+        <h3>
           <?php echo esc_html( $bellow['title'] ); ?>
         </h3>
-        <?php echo selleradise_widgets_svg('material/chevron-down'); ?>
       </button>
 
       <div 
-        class="description"
+        class="selleradise_Accordion__description"
         x-ref="description<?php echo esc_attr($key); ?>"
-        id="selleradiseAccordion--description-<?php echo esc_attr($key); ?>"
+        id="selleradise_Accordion__description-<?php echo esc_attr($key); ?>"
         role="region"
-        aria-labelledby="selleradiseAccordion--trigger-<?php echo esc_attr($key); ?>"
+        aria-labelledby="selleradise_Accordion__trigger-<?php echo esc_attr($key); ?>"
         x-show="opened === '<?php echo esc_attr($key); ?>'"
         <?php echo selleradise_get_alpine_transition_names(); ?> >
 
